ebba1c2bf3ecff050ea0d9446b78fce3e41f124d
unknown <axelpicaut@orange.fr>
2023-03-29 13:54:59 +0200

    mis en place previous/next

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,72 @@ import PokemonCard from './components/PokemonCard';
 
 const pokemonList = [
   {
+    index: "1",
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    index: "2",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    index: "3",
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    index: "4",
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    index: "5",
     name: "mew",
   },
 ];
 
 
-
 function App() {
+
+
+  const [count, setCount] = useState(0)
+
+
+  const handleClickPre = () => {
+    setCount(count - 1)
+  }
+  const handleClickNex = () => {
+    setCount(count + 1)
+  }
+
+
+
+
 
 
   return (
 
+
     <div>
-      <PokemonCard name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+
+      <PokemonCard name={pokemonList[count].name} imgSrc={pokemonList[count].imgSrc} />
+
+
+
+      <button onClick={handleClickPre}>Previous</button>
+      <button onClick={handleClickNex}>Next</button>
     </div>
   );
-
-
-
-
 }
+
+
+
+
+
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function PokemonCard(props) {
+function PokemonCard(pokemonList) {
   // const pokemon = props.pokemonList[0];
-  if (props.imgSrc) {
+  if (pokemonList.imgSrc) {
     return <figure>
-      <img src={props.imgSrc} alt={props.name}
+      <img src={pokemonList.imgSrc} alt={pokemonList.name}
         className='card-img' />
-      <figcaption >{props.name}</figcaption>
+      <figcaption >{pokemonList.name}</figcaption>
     </figure>
   } else {
     return <div>
       <p>???</p>
-      <figcaption >{props.name}</figcaption>
+      <figcaption >{pokemonList.name}</figcaption>
 
     </div>
 
   }
 
-
-
-
 };
+
+
+PokemonCard.PropTypes = {
+  pokemonList: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired).isRequired,
+}
+
+
 
 
 
